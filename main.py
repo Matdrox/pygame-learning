d39db3d627bb9e5c3fdd3126352f413ce5c68a8e
Matdrox <Matdrox@users.noreply.github.com>
2022-05-09 13:12:51 +0200

    Finished Setup

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import pygame, sys
 from pygame.locals import *
+
+clock = pygame.time.Clock()
+
 pygame.init()
 
+pygame.display.set_caption('Mytholos')
+
+WINDOW_SIZE = (800, 600)
+
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            print()
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(144)
