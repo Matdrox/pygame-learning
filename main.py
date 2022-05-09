e1e075024a089daae1605523931ac9de5545c8b5
Matdrox <Matdrox@users.noreply.github.com>
2022-05-09 14:31:46 +0200

    Added Sprite

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ pygame.display.set_caption('Mytholos')
 
 WINDOW_SIZE = (800, 600)
 
-screen = pygame.display.set_mode(WINDOW_SIZE)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
+player_image = pygame.transform.scale(pygame.image.load('img/triton.png'), (128, 128))
 
 while True:
+    screen.blit(player_image, (50, 50))
+
     for event in pygame.event.get():
         if event.type == QUIT:
-            print()
             pygame.quit()
             sys.exit()
 
