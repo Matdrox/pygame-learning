10b73ad52ac9b105a233c0be030f246ec2dad73e
Matdrox <Matdrox@users.noreply.github.com>
2022-05-11 18:30:23 +0200

    Updated Smooth Camera

--- levels/level.py
+++ levels/level.py
@@ -25,19 +25,24 @@ class Camera_group_y(pygame.sprite.Group):
         self.display_surface = pygame.display.get_surface()
         self.w = self.display_surface.get_size()[0] // 2.
         self.h = self.display_surface.get_size()[1] // 2
-        self.offset = pygame.math.Vector2()
+        self.camera = pygame.math.Vector2()
 
         self.floor = pygame.image.load('img/maps/floor.png').convert()
         self.floor_rect = self.floor.get_rect()
 
     def draw(self, player):
-        self.offset.x = player.rect.centerx - self.w
-        self.offset.y = player.rect.centery - self.h
+        # self.camera.x = player.rect.centerx - self.w
+        # self.camera.y = player.rect.centery - self.h
 
-        floor_offset_pos = self.floor_rect.topleft - self.offset
+        floor_offset_pos = self.floor_rect.topleft - self.camera
         self.display_surface.blit(self.floor, floor_offset_pos)
+
+        heading = player.rect.center - self.camera
+        self.camera += heading * 0.05
+        offset = -self.camera + pygame.math.Vector2(self.w, self.h)
 
         for sprite in self.sprites():
         # for sprite in sorted(self.sprites(), key= lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+            offset_pos = sprite.rect.topleft - self.camera
+            self.display_surface.blit(sprite.image, player.rect.topleft + offset)
+            # self.display_surface.blit(sprite.image, offset_pos)
--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                    sys.exit()
 
             self.screen.fill(0)
             self.level.run()
