2ea8892806a07b3563f4a788c1532bc66a9461e6
Matei Cananau <mateicananau@gmail.com>
2022-06-27 23:45:35 +0200

    Updated Comments

--- player.py
+++ player.py
@@ -18,16 +18,16 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.direction.x = -1
+            self.direction.x = -1  # Update for grid-based movement
         elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.direction.x = 1
+            self.direction.x = 1  # Update for grid-based movement
         else:
             self.direction.x = 0
 
         if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.direction.y = -1
+            self.direction.y = -1  # Update for grid-based movement
         elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.direction.y = 1
+            self.direction.y = 1  # Update for grid-based movement
         else:
             self.direction.y = 0
 
